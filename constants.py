163f897eda5aedf5a9d88155cccbad97052a4501
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-09-23 20:44:58 +0000

    added pizza calculator program

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created by Marwan Mashaly
+# Created on September 2019
+# This module contains constants for pizza program
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
